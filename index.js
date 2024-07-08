8e4c87bd26ff1f4a9b9fc75e4b8e4014dc2333c7
Triggered41 <vehdathamid@gmail.com>
2024-07-08 21:13:06 +0530

    Example is working now, updates the package version in right after the last commit

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+
+import Syntax from './Syntax.js';
+// import Syntax from '../src';
+// import Syntax from '../src';
+
 // import Syntax from 'editorjs-syntax-hljs'
-console.log(EditorJS)
+
+console.log("Hello: ", Syntax)
+
 const editor = new EditorJS({
     holder: 'EditorID',
-    code: {
+    tools:{
+        code: {
+            class: Syntax,
+            config: {
 
+                languages: [
+                    {key: 'xml', label: 'HTML/XML'},
+                    {key: 'python', label: 'Python'},
+                    {key: 'java', label: 'Java'},
+                    {key: 'csharp', label: 'C#'},
+                    {key: 'javascript', label: 'Javascript'},
+                    {key: 'rust', label: 'Rust'},
+                    {key: 'bash', label: 'Bash'},
+                ]
+            }
+        }
     }
 })
